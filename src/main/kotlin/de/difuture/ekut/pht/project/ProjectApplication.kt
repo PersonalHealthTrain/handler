07d875e71fd9b964148ff3007426ba58b13344be
Lukas Zimmermann <luk.zim91@gmail.com>
2018-10-31 18:43:58 +0100

    Init: Project stub

---
+++ src/main/kotlin/de/difuture/ekut/pht/project/ProjectApplication.kt
@@ -0,0 +1,11 @@
+package de.difuture.ekut.pht.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectApplication>(*args)
+}
